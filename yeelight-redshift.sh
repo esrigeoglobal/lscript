cc172bc84833bfbfd0b213fc79fb0ecb623de187
JackobZa <elias.jacob405@gmail.com>
2026-09-01 12:48:00 -0600

    systemd -> Sep 2026 v1

--- yeelight-redshift.sh
+++ yeelight-redshift.sh
@@ -7,3 +7,21 @@ bright=$(python -c "print(round($temp*0.02-30))")
 temp=$(python -c "print(round($temp*1.75-4875))")  #adjust constants for your preference
 $( dirname $0 )/yeelight-colortemp.sh 0 $temp
 $( dirname $0 )/yeelight-brightness.sh 0 $bright
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+sudo journalcto --user anyuser -u apache2.service
